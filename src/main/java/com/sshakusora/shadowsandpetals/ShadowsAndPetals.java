d058770603c8055181372358c1d996efea9938f7
笏天 <1404928002@qq.com>
2026-05-02 22:57:46 +0800

    initial comment

---
+++ src/main/java/com/sshakusora/shadowsandpetals/ShadowsAndPetals.java
@@ -0,0 +1,11 @@
+package com.sshakusora.shadowsandpetals;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(ShadowsAndPetals.MOD_ID)
+public class ShadowsAndPetals {
+    public static final String MOD_ID = "shadowsandpetals";
+    private static final Logger LOGGER = LogUtils.getLogger();
+}
